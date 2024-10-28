3c95b0d5b1b310a1f74b679042f910d3570946c9
nicholas-middelberg <102709066+nicholas-middelberg@users.noreply.github.com>
2024-10-27 20:30:37 -0400

    created new basic app store app template to work on

---
+++ wav-app/wav-app/wav_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  wav_appApp.swift
+//  wav-app
+//
+//  Created by Nicholas Middelberg on 10/27/24.
+//
+
+import SwiftUI
+
+@main
+struct wav_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ wav-app/wav-appTests/wav_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  wav_appTests.swift
+//  wav-appTests
+//
+//  Created by Nicholas Middelberg on 10/27/24.
+//
+
+import Testing
+@testable import wav_app
+
+struct wav_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
