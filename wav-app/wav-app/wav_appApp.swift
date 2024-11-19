5159f904ecbec0d245b8b3c623e10b9ede3509d2
nicholas-middelberg <102709066+nicholas-middelberg@users.noreply.github.com>
2024-11-19 02:12:12 -0500

    revert some of the changes used for testing a new UI

--- wav-app/wav-app/wav_appApp.swift
+++ wav-app/wav-app/wav_appApp.swift
@@ -14,7 +14,7 @@ struct wav_appApp: App {
     var body: some Scene {
         WindowGroup {
             if isLoggedIn {
-                TestView() // Show the home view
+                HomeView() // Show the home view
             } else {
                 OnboardingView() // Show view to create an account
             }
